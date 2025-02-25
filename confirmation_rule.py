5dab8305e52b76d932e8ab8da4390dda919c4b1a
Roberto Saltini <saltini.roberto@gmail.com>
2025-02-25 18:50:45 +1100

    Fix bugs

--- confirmation_rule.py
+++ confirmation_rule.py
@@ -127,7 +127,7 @@ def find_latest_confirmed_descendant(store, latest_confirmed_root) -> Root:
         if (block_epoch > compute_epoch_at_slot(latest_confirmed_slot)
             and 
                 (not will_checkpoint_be_justified(store, checkpoint) or
-                store.unrealized_justifications[get_head(store)].epoch + 1 == current_epoch)
+                store.unrealized_justifications[get_head(store)].epoch + 1 >= current_epoch)
             ):
             break
             
